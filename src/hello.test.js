63501d5b55f3af8cd9da948a4107087acd9dea39
kamataryo <kamataryo@users.noreply.github.com>
2020-05-23 17:18:24 +0900

    Some fix

--- src/hello.test.js
+++ src/hello.test.js
@@ -1,14 +1,13 @@
 const { dependencies, handler } = require("./hello.js");
 const test = require("ava");
 
-test("should success", async (t) => {
+test("should success without DI", async (t) => {
   await handler({}, {}, (_0, res) => {
     t.is(res.statusCode, 200);
   });
 });
 
-test("should fail", async (t) => {
-  // dependencies injection
+test("should fail with negative DI", async (t) => {
   dependencies.saas = () => false;
   await handler({}, {}, (_0, res) => {
     t.is(res.statusCode, 500);
